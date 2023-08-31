50c05d7a3af89a76cf3e9b4932780fb2898dd0b5
fm131410 <18588605834@163.com>
2023-08-31 14:46:42 +0800

    vue2-dateV

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <dv-full-screen-container>
+
+
+
+
+
+
+  </dv-full-screen-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
 </script>
